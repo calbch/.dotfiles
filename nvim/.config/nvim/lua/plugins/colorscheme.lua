e62774b761cf5f8ad8f32c833646f5edb3d72ecb
caldotdev <cluckenbach@protonmail.com>
2023-12-30 11:19:23 +0100

    move nvim to 'stowable' folder

---
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+  { "rose-pine/neovim", lazy = true, name = "rose-pine", opts = {
+    style = "moon",
+  } },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "rose-pine",
+    },
+  },
+}
